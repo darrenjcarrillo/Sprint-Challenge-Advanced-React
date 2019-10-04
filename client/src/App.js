db248bda63f6be71bae48ccfedde69170d881e88
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-04 11:12:09 -0700

    1 test working

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <h1>Players</h1>
       <ClassComponent />
     </div>
   );
--- client/src/components/components.test.js
+++ client/src/components/components.test.js
@@ -4,15 +4,15 @@ import ClassComponent from "./ClassComponent";
 import Display from "./Display";
 import App from "../App";
 
-test("this will render without crashing", () => {
-  render(<App />);
-});
-
-// test("I have a Player Text inside of Display component", () => {
-//   const { getByText } = render(<Display />);
-
-//   getByText("Player");
+// test("this will render without crashing", () => {
+//   render(<Display />);
 // });
+
+test("I have a Players Text inside of Display component", () => {
+  const { getByText } = render(<App />);
+
+  getByText("Players");
+});
 
 // test("Players header is found", () => {
 //   const { getByTestId } = render(<Display />);
